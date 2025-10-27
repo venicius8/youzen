ca7bd8b7d7c7c6e6419c46875addd3234e8ebf85
Venícius <miguelsilva8v@gmail.com>
2025-10-27 10:20:05 -0300

    feat: add header

---
+++ app/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-gray-200 flex justify-between items-center top-0 w-screen h-18 z-100 shadow-xl sticky">
+      <div>
+        <h1 className="text-4xl">LogoEx</h1>
+      </div>
+      <nav>
+        <ul className="flex gap-4 bg-white">
+          <li>
+            <Link href="/">aEx</Link>
+          </li>
+          <li>
+            <Link href="/">bEx</Link>
+          </li>
+          <li>
+            <Link href="/">cEx</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
