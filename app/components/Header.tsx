5bdad0c8221b841d3a1935ff00e975afa2f2dbbf
Venícius <miguelsilva8v@gmail.com>
2025-10-27 19:03:10 -0300

    feat: finish main page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,22 +1,29 @@
 import Link from "next/link";
 
+const navLink = (el: string, ref: string) => (
+  <li>
+    <Link
+      href={ref}
+      className="bg-white text-xl p-2 rounded-xl hover:border-b-2 duration-50"
+    >
+      {el}
+    </Link>
+  </li>
+);
+
 export default function Header() {
   return (
-    <header className="bg-gray-100 flex justify-between items-center top-0 w-screen h-16 z-100 shadow-2xl sticky">
+    <header className="bg-gray-100 flex justify-between items-center top-0 w-screen h-16 z-100 shadow-2xl sticky px-10">
       <div>
-        <h1 className="text-4xl">LogoEx</h1>
+        <Link href={"/"} className="text-4xl">
+          LogoEx
+        </Link>
       </div>
       <nav>
-        <ul className="flex gap-4 bg-white">
-          <li>
-            <Link href="/">aEx</Link>
-          </li>
-          <li>
-            <Link href="/">bEx</Link>
-          </li>
-          <li>
-            <Link href="/">cEx</Link>
-          </li>
+        <ul className="flex gap-8">
+          {navLink("Explorar", "/")}
+          {navLink("Sobre", "/")}
+          {navLink("Perfil", "/")}
         </ul>
       </nav>
     </header>
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ export default function name() {
   const newUser: boolean = true;
 
   return (
-    <main className="bg-green-100 h-300 text-center">
+    <main className="bg-green-100 h-screen text-center">
       <div className="relative bg-[url('/morning-nature.jpg')] h-[50vh] bg-cover bg-center flex items-center justify-center mb-10">
         <h1 className="text-4xl text-white bg-black/30 px-4 py-2 rounded-2xl z-10">
           Bom dia
         </h1>
-        <span className="absolute inset-x-0 bottom-0 h-1/3 bg-linear-to-b from-transparent via-transparent to-green-100"></span>
+        <span className="absolute inset-x-0 bottom-0 h-6 bg-linear-to-b from-transparent to-green-100"></span>
       </div>
 
       {newUser ? (
