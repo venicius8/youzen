eb63299ac2f2fd9fbe7cfcc162e838e078f0c64c
Venícius <miguelsilva8v@gmail.com>
2025-11-05 20:58:32 -0300

    feat: add HorizontalScrollView component

---
+++ app/components/UI/HorizontalScrollView.tsx
@@ -0,0 +1,16 @@
+function HorizontalScrollView({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title: string;
+}) {
+  return (
+    <section>
+      <h2 className="text-3xl font-bold ml-4">{title}</h2>
+      <div className="w-screen h-40 mb-10 flex gap-4 overflow-x-scroll">
+        {children}
+      </div>
+    </section>
+  );
+}
