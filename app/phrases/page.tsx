6e3cb96aff0dbb3be1da8cc44df0a049dd2f1acb
Venícius <miguelsilva8v@gmail.com>
2025-11-06 20:46:17 -0300

    fix: remove redundancy code

--- app/phrases/page.tsx
+++ app/phrases/page.tsx
@@ -2,39 +2,35 @@
 
 import { useEffect, useState, useRef } from "react";
 import Phrases from "./Phrases";
+import { addTime, getTime, saveTime } from "@/utils/timeTracker";
 
 export default function Phrase() {
-  const [firstClick, setFirstClick] = useState(false);
   const [currentPhrase, setCurrentPhrase] = useState("");
   const [allPhrases, setAllPhrases] = useState<string[]>([]);
-
-  const clickHandlerRef = useRef<(() => void) | null>(null);
+  const [seconds, setSeconds] = useState(0);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    const loadPhrases = async () => {
-      const data = Phrases;
-      setAllPhrases(data);
-    };
-
-    loadPhrases();
+    setAllPhrases(Phrases);
   }, []);
 
   useEffect(() => {
     if (allPhrases.length === 0) return;
 
-    const handleFirstClick = () => {
-      setFirstClick(true);
-      nextPhrase();
-    };
+    setSeconds(getTime("phrases"));
 
-    clickHandlerRef.current = handleFirstClick;
-    window.addEventListener("click", handleFirstClick);
+    intervalRef.current = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+      console.log(seconds);
+    }, 1000);
 
     return () => {
-      if (clickHandlerRef.current)
-        window.removeEventListener("click", clickHandlerRef.current);
+      if (intervalRef.current) clearInterval(intervalRef.current);
+
+      const totalTime = addTime("phrases", seconds);
+      saveTime("phrases", totalTime);
     };
-  }, [firstClick, allPhrases]);
+  }, [allPhrases]);
 
   const nextPhrase = () => {
     const randomIndex = Math.floor(Math.random() * allPhrases.length);
@@ -44,23 +40,21 @@ export default function Phrase() {
   };
 
   const handleLocalClick = () => {
-    if (firstClick) {
-      nextPhrase();
-    }
+    nextPhrase();
   };
 
   return (
     <section className="h-screen flex flex-row">
       <div
         className="bg-blue-300/10 w-1/4"
-        onClick={() => setCurrentPhrase(allPhrases[-1])}
+        onClick={() => setCurrentPhrase(allPhrases[0])}
       ></div>
       <div className="bg-blue-500/10 w-3/4" onClick={handleLocalClick}></div>
       <span
         className="fixed left-1/2 top-1/2 -translate-1/2 text-4xl text-center w-4/5 rounded-2xl bg-white/30 p-4 md:w-1/3"
         onClick={(e) => e.stopPropagation()}
       >
-        {currentPhrase || "Clique em qualquer lugar para começar"}
+        {currentPhrase || `Clique em qualquer lugar para começar`}
       </span>
     </section>
   );
