180c7bedae713242140f0ef6621e64f0159aa142
Venícius <miguelsilva8v@gmail.com>
2025-11-14 16:12:41 -0300

    feat(profile): add more details access section

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,10 +3,16 @@
 import { allFeatures } from "@/utils/allFeatures";
 import { getResume } from "@/utils/timeTracker";
 import { useState, useEffect } from "react";
+import Overlay from "../components/UI/Overlay";
 
 export default function Profile() {
   const [totalSpentTime, setTotalSpentTime] = useState<Record<string, number>>(
     {}
+  );
+  const [currentFeature, setCurrentFeature] = useState("");
+
+  const currentFeatureObj = allFeatures.find(
+    (el) => el.nick === currentFeature
   );
 
   useEffect(() => {
@@ -35,9 +41,13 @@ export default function Profile() {
         const feature = allFeatures.find((el) => el.nick === nick);
 
         return (
-          <div key={nick} className="flex items-center gap-2">
+          <div
+            key={nick}
+            className="flex items-center gap-4 mt-4 cursor-pointer w-full bg-linear-to-br from-blue-200 to-blue-300 rounded"
+            onClick={() => setCurrentFeature(nick)}
+          >
             <div
-              className="w-30 aspect-square bg-cover"
+              className="w-30 aspect-square bg-cover border-r border-white"
               style={{ backgroundImage: `url(${feature?.imageURL})` }}
               role="img"
             ></div>
@@ -48,6 +58,21 @@ export default function Profile() {
           </div>
         );
       })}
+      {currentFeature && (
+        <>
+          <dialog
+            open
+            className="w-4/5 min-h-96 bg-white fixed left-1/2 top-1/2 -translate-1/2 p-6 rounded-xl z-110"
+          >
+            <h1 className="text-3xl">
+              Detalhes sobre {currentFeatureObj?.featureName}
+            </h1>
+            <p>{currentFeatureObj?.about}</p>
+            <p></p>
+          </dialog>
+          <Overlay onClick={() => setCurrentFeature("")} zIndex={100} />
+        </>
+      )}
     </section>
   );
 }
