3c4fb676f60246c4333f5eb57a7df385bbf5fc3c
Venícius <miguelsilva8v@gmail.com>
2025-11-24 14:39:21 -0300

    fix(Phrases): improve semantic and fix click error

--- app/phrases/page.tsx
+++ app/phrases/page.tsx
@@ -39,6 +39,7 @@ export default function Phrase() {
   };
 
   const handleLocalClick = () => {
+    if (isAnimating) return;
     setIsAnimating(true);
     setTimeout(() => {
       setIsAnimating(false);
@@ -47,20 +48,19 @@ export default function Phrase() {
   };
 
   return (
-    <section className="h-screen flex flex-row">
+    <section className="h-screen flex flex-row" onClick={handleLocalClick}>
+      <span className="bg-blue-300/10 w-1/4"></span>
+      <span className="bg-blue-500/10 w-3/4"></span>
       <div
-        className="bg-blue-300/10 w-1/4"
-        onClick={() => setCurrentPhrase(allPhrases[0])}
-      ></div>
-      <div className="bg-blue-500/10 w-3/4" onClick={handleLocalClick}></div>
-      <span
         className={`fixed left-1/2 top-1/2 -translate-1/2 text-4xl
           text-center w-4/5 rounded-2xl bg-white/30 p-4 md:w-1/3
           duration-700 ${isAnimating ? "opacity-0" : "opacity-100"}`}
         onClick={(e) => e.stopPropagation()}
       >
-        {currentPhrase || `Clique em qualquer lugar ao redor para começar`}
-      </span>
+        <p>
+          {currentPhrase || `Clique em qualquer lugar ao redor para começar`}
+        </p>
+      </div>
     </section>
   );
 }
